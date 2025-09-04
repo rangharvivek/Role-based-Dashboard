161b9aa3972fd86cf52632a8d87cc09a8d25012b
rangharvivek <rangharvivek003@gmail.com>
2025-09-04 06:44:11 +0100

    Update dashboard.js

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,14 +4,12 @@ const { ensureAuth, checkRole } = require("../middleware/authMiddleware");
 const User = require("../models/User");
 const Post = require("../models/Post");
 
-// admin dashboard
 router.get("/admin", ensureAuth, checkRole(["admin"]), async (req, res) => {
   const users = await User.find().select("-password").lean();
   const posts = await Post.find().populate("author","username").lean();
   res.render("dashboard/admin", { user: req.session.user, users, posts, messages: res.locals.messages });
 });
 
-// author dashboard
 router.get("/author", ensureAuth, checkRole(["author","admin"]), async (req, res) => {
   const myPosts = await Post.find({ author: req.session.user.id }).lean();
   res.render("dashboard/author", { user: req.session.user, posts: myPosts, messages: res.locals.messages });
