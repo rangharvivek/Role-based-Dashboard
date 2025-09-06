f3deb790a0cadfe3ed7f588cfb627a9b35f40e7e
rangharvivek <rangharvivek003@gmail.com>
2025-09-06 06:15:06 +0100

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const postSchema = new mongoose.Schema({
   title: { type: String, required: true, trim: true },
   content: { type: String },
-  image: { type: String }, // stored as /uploads/filename or just filename depending on usage
+  image: { type: String }, 
   author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   categories: [String],
   tags: [String],
